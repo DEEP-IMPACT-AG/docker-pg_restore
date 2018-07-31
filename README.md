59c603ff2365c5d518ee412cb3fe4945202f5a0e
Stanislas Nanchen <stan@hood.ch>
2018-07-31 15:54:48 +0200

    decrypt

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # docker-pg_restore
 
 Alpine image with postgres client to restore a custom format dump
+
+You have to mount the private key as a volume `/var/backup_key.pem`.
--- restore.sh
+++ restore.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-pg_restore -U ${PGUSER} -h ${PGHOST} -d ${PGDB} /var/dump
+openssl smime -decrypt -in /var/dump.ssl -binary \
+  -inform DEM -inkey /var/backup_key.pem | pg_restore -U ${PGUSER} -h ${PGHOST} -d ${PGDB} 
