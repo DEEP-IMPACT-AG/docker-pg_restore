403f2e34c5d81dd44b29e980ac66b94468ac6e9f
Stanislas Nanchen <stan@hood.ch>
2018-09-07 16:43:40 +0200

    pipefail to stop if decrypting is not successful

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -eo pipefail
 
 openssl smime -decrypt -in /var/dump.ssl -binary \
   -inform DEM -inkey /var/backup_key.pem | pg_restore -U ${PGUSER} -h ${PGHOST} -d ${PGDB} 
