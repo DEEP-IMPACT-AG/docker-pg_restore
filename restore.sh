a1b8899df7d7a157e736d8c5a6a6729c8642df5b
Stanislas Nanchen <stan@hood.ch>
2018-09-08 09:54:50 +0200

    allow to pass commands to pg_restore.

--- restore.sh
+++ restore.sh
@@ -3,4 +3,4 @@
 set -eo pipefail
 
 openssl smime -decrypt -in /var/dump.ssl -binary \
-  -inform DEM -inkey /var/backup_key.pem | pg_restore -U ${PGUSER} -h ${PGHOST} -d ${PGDB} 
+  -inform DEM -inkey /var/backup_key.pem | pg_restore -U ${PGUSER} -h ${PGHOST} -d ${PGDB} "$@"
